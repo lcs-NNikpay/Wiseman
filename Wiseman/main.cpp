04c9d3d3fdbd0be2be13fa4bab2dbf1b72b1c820
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-12-13 14:26:57 -0500

    Initial Commit

---
+++ Wiseman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wiseman
+//
+//  Created by Nikpay, Nicki on 2017-12-13.
+//  Copyright © 2017 Nikpay, Nicki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
